67c7d1162caef415cdd403d278ac9bca3ad30009
Mohamed Hammad <mhammad101088@gmail.com>
2021-10-23 20:38:07 +0200

    update add user method and employee name test data

--- src/main/java/gui/pageObjectModel/AddUserPage.java
+++ src/main/java/gui/pageObjectModel/AddUserPage.java
@@ -2,6 +2,7 @@ package gui.pageObjectModel;
 
 import com.shaft.gui.element.ElementActions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 public class AddUserPage {
